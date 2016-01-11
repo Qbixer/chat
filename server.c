9f4effcb1237eda58291adae01c2e61adfcc59cf
dzejkop <kbchojnacki@gmail.com>
2016-01-11 19:28:16 +0100

    p

--- server.c
+++ server.c
@@ -2,20 +2,19 @@
 #define LOGIN 12
 #define POKOJE 5
 #define WIADOMOSC 128
-char zajeta[USERS];
-char login[USERS][LOGIN];
-
-struct pokoj{
-char nazwa[LOGIN];
-int id;
-int ilosc;
-};
 
 #include <stdio.h>
 #include <string.h>
 
+struct pokoj{
+	char nazwa[LOGIN];
+	int id;
+	int ilosc;
+	int uzytkownicy[USERS];
+};
 
-
+char zajety[USERS];
+char login[USERS][LOGIN];
 char send[WIADOMOSC];
 char get[WIADOMOSC];
 struct pokoj pokoje[POKOJE];
@@ -40,38 +39,37 @@ int main(){
 
 char rozkaz[6];
 
-while(1){
-	memset(get,0,sizeof(char));
+while(1) {
+	memset(get, 0, sizeof(char));
 	get();
-	strncpy(get, rozkaz+2,6);
-	if(strcmp(rozkaz,"\login")){
+	strncpy(get, rozkaz + 2, 6);
+	if (strcmp(rozkaz, "/login")) {
 		logowanie();
 
-	} else if(strcmp(rozkaz,"\lgout")){
+	} else if (strcmp(rozkaz, "/lgout")) {
 		logout();
-	} else if(strcmp(rozkaz,"\rlist")){
+	} else if (strcmp(rozkaz, "/rlist")) {
 
-	} else if(strcmp(rozkaz,"\ulist")){
+	} else if (strcmp(rozkaz, "/ulist")) {
 
-	} else if(strcmp(rozkaz,"\lgout")){
+	} else if (strcmp(rozkaz, "/lgout")) {
 
-	} else if(strcmp(rozkaz,"\eroom")){
+	} else if (strcmp(rozkaz, "/eroom")) {
 
-	} else if(strcmp(rozkaz,"\lroom")){
+	} else if (strcmp(rozkaz, "/lroom")) {
 
-	} else if(strcmp(rozkaz,"\croom")){
+	} else if (strcmp(rozkaz, "/croom")) {
 
-	}else if(strcmp(rozkaz,"\mroom")){
+	} else if (strcmp(rozkaz, "/mroom")) {
 
-	}else if(strcmp(rozkaz,"\muser")){
+	} else if (strcmp(rozkaz, "/muser")) {
 
 	}
-	else{
-		for(int i = 0; i<USERS;i++){
-			
-			strncpy(send, get,128);
-			send(i+3);
-		}	
-	}
+	else {
+		for (int i = 0; i < USERS; i++) {
 
+			strncpy(send, get, 128);
+			send(i + 3);
+		}
+	}
 }
